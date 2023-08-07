c804b0d46d9e3814b5d4b3df2ce99a3b1789fb9a
Lea Rosema <lea@lea.codes>
2023-08-07 22:47:47 +0200

    feat: add AnimationLoop class

---
+++ src/game/utils/animation-interval.ts
@@ -0,0 +1,54 @@
+export interface AnimationInterval {
+  callback: FrameRequestCallback;
+  timeout: number;
+  elapsed: number;
+  calls: number;
+}
+
+export class AnimationLoop {
+  lastTimestamp = 0;
+  timer = NaN;
+
+  intervals: AnimationInterval[] = [];
+
+  get running() {
+    return (! Number.isNaN(this.timer));
+  }
+
+  add(callback: FrameRequestCallback, timeout: number) {
+    this.intervals.push({
+      callback,
+      timeout,
+      elapsed: 0,
+      calls: 0,
+    });
+  }
+
+  run() {
+    if (Number.isNaN(this.timer)) {
+      this.lastTimestamp = -Infinity;
+      this.timer = requestAnimationFrame(this.loop);
+    }
+  }
+
+  stop() {
+    if (! Number.isNaN(this.timer)) {
+      cancelAnimationFrame(this.timer);
+      this.timer = NaN;
+    }
+  }
+
+  loop = (t: DOMHighResTimeStamp) => {
+    const timePassed = (t - this.lastTimestamp);
+    for (const interval of this.intervals) {
+      interval.elapsed += timePassed;
+      if (interval.elapsed >= interval.timeout) {
+        interval.calls += 1;
+        interval.elapsed = 0;
+        interval.callback(t);
+      }
+    }
+    this.lastTimestamp = t;
+    this.timer = requestAnimationFrame(this.loop);
+  }
+}
