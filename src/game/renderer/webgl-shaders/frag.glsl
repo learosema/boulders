c24331fec3cab03a2445d8e2e9e72b2e2ceba076
Lea Rosema <lea@lea.codes>
2023-09-11 23:09:11 +0200

    chore: access levelTexture

--- src/game/renderer/webgl-shaders/frag.glsl
+++ src/game/renderer/webgl-shaders/frag.glsl
@@ -1,9 +1,11 @@
 precision highp float;
 
 uniform vec2 resolution;
+uniform sampler2D levelTexture;
+varying vec2 vUv;
 
 void main() {  
   vec2 pos = gl_FragCoord.xy;
-
-  gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+  vec4 level = texture2D(levelTexture, vUv);
+  gl_FragColor = vec4(level.rgb * 30., 1.);
 }
