b22af6519c09c3bb2374246f1e879d53f5f8088b
Lea Rosema <lea@lea.codes>
2023-10-04 21:49:35 +0200

    chore: add a basic test for loadImage

---
+++ src/game/utils/load-image.test.ts
@@ -0,0 +1,15 @@
+import { loadImage } from "./load-image";
+
+describe('loadImage function', () => {
+
+  // data-URL of a transparent pixel.
+  const DATA_URL = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+
+  it('loads an image', async () => {
+    const image = await loadImage(DATA_URL);
+    expect(image).toBeInstanceOf(HTMLImageElement);
+    expect(image.width).toBe(1);
+    expect(image.height).toBe(1);
+  });
+
+})
