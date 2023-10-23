13520432643fdd37d9a4017dbcea3a918f9460fa
Lea Rosema <lea.rosema@novatec-gmbh.de>
2023-10-23 12:07:33 +0200

    chore: add jsdoc

--- src/game/utils/num-utils.ts
+++ src/game/utils/num-utils.ts
@@ -1,7 +1,19 @@
+/**
+ * Makes an integer number odd.
+ * @param n an arbitrary integer value
+ * @returns (n + 1) when the number is even, else n
+ */
 export function oddly(n: number) {
   return n % 2 === 0 ? (n + 1) : n;
 }
 
+/**
+ * Clamps the a number between a min and a max limit
+ * @param n an arbitrary value
+ * @param min minimum limit
+ * @param max maximum limit
+ * @returns clamped value
+ */
 export function clamp(n: number, min: number, max: number) {
   return Math.min(Math.max(n, min), max);
 }
